14d1e6d03531068947bb348dd2befcff22105a2e
Giorgi Kheladze <157839203+giorgi-kheladze@users.noreply.github.com>
2025-02-02 03:06:44 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+function showPassword(){
+    let passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+}
+
+function forgetPassword(){
+    let forgetPassword = document.getElementById("forget-password");
+    let active = document.getElementById("sign-in");
+    let register = document.getElementById("authoritation")
+
+    forgetPassword.style.display = "block";
+    active.style.display = "none";
+    register.style.display = "block";
+}
+
+function showAuthorization(){
+    let forgetPassword = document.getElementById("forget-password");
+    let active = document.getElementById("sign-in");
+    let register = document.getElementById("authoritation")
+    let resolve = document.getElementById("resolve-password")
+
+    forgetPassword.style.display = "none";
+    active.style.display = "flex";
+    register.style.display = "none";
+    resolve.style.display = "none";
+}
+
+function resolvePassword(){
+    let forgetPassword = document.getElementById("forget-password");
+    let active = document.getElementById("sign-in");
+    let register = document.getElementById("authoritation")
+    let resolve = document.getElementById("resolve-password")
+
+    forgetPassword.style.display = "none";
+    active.style.display = "none";
+    register.style.display = "none";
+    resolve.style.display = "flex";
+}
---
+++ validation.js
@@ -0,0 +1,4 @@
+const form = 0;
+const email_input = 1;
+const password_input = 0;
+const reset_password_input = 0;
